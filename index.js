2666857b69c05fc24ddd063d72be85d93732bae2
pktai <pktai.iot@gmail.com>
2019-01-15 22:43:42 +0700

    update sendfile postman success

--- index.js
+++ index.js
@@ -12,19 +12,46 @@ const drive = google.drive({
     version: 'v3',
     auth: oauth2Client
 });
-async function main() {
+async function main(fileName) {
     const res = await drive.files.create({
         requestBody: {
-            name: 'name.pdf',
+            name: fileName,
             mimeType: 'application/pdf'
         },
         media: {
             mimeType: 'application/pdf',
-            body: fs.createReadStream('name.pdf')
+            body: fs.createReadStream(fileName)
         }
     })
     console.log(res.data.id);
     return res.data.id;
 };
 
-main();
+
+var express = require('express')
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+var app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
+    next();
+});
+
+app.post('/fileupload', upload.single('streamfile'),async function (req, res) {
+    // let stream = fs.createReadStream(req.file)
+
+    var a = await main(req.file.path)
+    console.log('req.file', a);
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+  res.end('success');
+})
+
+// Connect to Web Server
+var port = '3000';
+app.listen(port, () => {
+    console.log('[Express] fileupload started at %d port', port)});
