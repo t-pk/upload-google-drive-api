d13f6d2c8b8600e890499bec26ca88a874c70241
Tai Pham <pktai.iot@gmail.com>
2019-06-22 22:47:45 +0700

    update config, fix package-lock.json

---
+++ config.example.js
@@ -0,0 +1,6 @@
+export default config = {
+  YOUR_CLIENT_ID: 'example',
+  YOUR_CLIENT_SECRET: 'example',
+  YOUR_REDIRECT_URL: 'example',
+  REFRESH_TOKEN: 'example'
+}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { google } = require('googleapis');
 const fs = require('fs');
 const config = require('./config');
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const app = express();
 
-
-var oauth2Client = new google.auth.OAuth2(config.YOUR_CLIENT_ID, config.YOUR_CLIENT_SECRET, config.YOUR_REDIRECT_URL);
+const oauth2Client = new google.auth.OAuth2(config.YOUR_CLIENT_ID, config.YOUR_CLIENT_SECRET, config.YOUR_REDIRECT_URL);
 oauth2Client.credentials = {
     refresh_token: config.REFRESH_TOKEN
 };
@@ -12,6 +15,7 @@ const drive = google.drive({
     version: 'v3',
     auth: oauth2Client
 });
+
 async function main(fileName) {
     const res = await drive.files.create({
         requestBody: {
@@ -27,31 +31,25 @@ async function main(fileName) {
     return res.data.id;
 };
 
-
-var express = require('express')
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
-
-var app = express()
-
-app.use(function(req, res, next) {
+app.use(function (_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
     next();
 });
 
-app.post('/fileupload', upload.single('streamfile'),async function (req, res) {
+app.post('/fileupload', upload.single('streamfile'), async function (req, res) {
     // let stream = fs.createReadStream(req.file)
     var as = req.body; // request infor text
     console.log(as);
     var a = await main(req.file.path) //request infor file
     console.log('req.file', a);
 
-  res.end('success');
+    res.end('success');
 })
 
 // Connect to Web Server
 var port = '3000';
 app.listen(port, () => {
-    console.log('[Express] fileupload started at %d port', port)});
+    console.log('[Express] fileupload started at %d port', port)
+});
