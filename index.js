7ffe3074d899fe725d67da1b0ca6eb92e98b705e
Tai Pham <36804773+pktai@users.noreply.github.com>
2020-01-11 10:29:50 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const { google } = require('googleapis');
-const fs = require('fs');
-const config = require('./config');
-const express = require('express');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-const app = express();
+const { google }    = require('googleapis');
+const fs            = require('fs');
+const express       = require('express');
+const multer        = require('multer');
+const config        = require('./config');
+const upload        = multer({ dest: 'uploads/' });
+const app           = express();
 
-const oauth2Client = new google.auth.OAuth2(config.YOUR_CLIENT_ID, config.YOUR_CLIENT_SECRET, config.YOUR_REDIRECT_URL);
+const oauth2Client = new google.auth.OAuth2(
+    config.YOUR_CLIENT_ID, 
+    config.YOUR_CLIENT_SECRET, 
+    config.YOUR_REDIRECT_URL
+);
 oauth2Client.credentials = {
     refresh_token: config.REFRESH_TOKEN
 };
@@ -33,18 +37,19 @@ async function main(fileName) {
 
 app.use(function (_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header(
+        "Access-Control-Allow-Headers", 
+        'Content-Type, 
+        Authorization, 
+        Content-Length, 
+        X-Requested-With'
+    );
     res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
     next();
 });
 
 app.post('/fileupload', upload.single('streamfile'), async function (req, res) {
-    // let stream = fs.createReadStream(req.file)
-    var as = req.body; // request infor text
-    console.log(as);
-    var a = await main(req.file.path) //request infor file
-    console.log('req.file', a);
-
+    await main(req.file.path) //request infor file
     res.end('success');
 })
 
